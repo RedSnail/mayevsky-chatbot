f2e2706f94a8de8311f963aa7035d777f55e0c07
glubshev2001 <glubshev2001@mail.ru>
2019-07-19 20:48:36 +0300

    autoversioning + dependencies fix

--- setup.py
+++ setup.py
@@ -6,10 +6,11 @@ install_requires=[
 
 setuptools.setup(
     name="mayevsky-chatbot",
-    use_scm_version = {"root": "..", "relative_to": __file__},
+    use_scm_version = True,
     setup_requires=['setuptools_scm'],    author="RedSnail",
     author_email="",
     description="The bot for TheBigMayevsky",
     url="https://github.com/RedSnail/mayevsky-chatbot",
-    packages=setuptools.find_packages()
+    packages=setuptools.find_packages(),
+    install_requires=install_requires
 )
