dd1707172c7814109e760057bc0b9ea044206eee
Glubshev Artem <36304382+SomeAnonimCoder@users.noreply.github.com>
2019-07-19 21:43:43 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 python3 -m pip install --upgrade setuptools wheel
 python3 setup.py sdist bdist_wheel
 python3 -m pip install --upgrade twine
-python3 -m twine upload dist/* --repository-url pypi.grihabor.com
+python3 -m twine upload dist/* --repository-url https://pypi.grihabor.com
 
