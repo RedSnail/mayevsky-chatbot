16b3f9363fd678dcb10d89c5f719a2f2cd32f4a7
Glubshev Artem <36304382+SomeAnonimCoder@users.noreply.github.com>
2019-07-19 17:29:58 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 python3 -m pip install --upgrade setuptools wheel
 python3 setup.py sdist bdist_wheel
 python3 -m pip install --upgrade twine
-python3 -m twine upload dist/* 
+echo TWINE_USERNAME TWINE_PASSWORD |python3 -m twine upload dist/* 
 
